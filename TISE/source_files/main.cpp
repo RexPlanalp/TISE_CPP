b94e1e3260708369dde54e9ff69fb921a529c053
Rex <dopl4670@colorado.edu>
2024-12-23 19:08:54 -0700

    Seems to be working, should actually physically check matrices at some point

--- TISE/source_files/main.cpp
+++ TISE/source_files/main.cpp
@@ -14,17 +14,31 @@ int main(int argc, char **argv) {
 
     ierr = SlepcInitialize(&argc, &argv, NULL,NULL); CHKERRQ(ierr);
 
-    int n_basis = 3000;
+    int n_basis = 1500;
     int degree = 6;
-    double rmax = 3000;
-    int lmax = 50;
+    double rmax = 1500;
+    int lmax = 75;
     int nmax = 10;
     double R0 = 1245;
     double eta=  0;
     double tise_tolerance = 1E-15;
     PetscInt tise_mat_iter = 3000;
 
+    // int n_basis = 3;
+    // int degree = 2;
+    // double rmax = 3;
+    // int lmax = 1;
+    // int nmax = 10;
+    // double R0 = 3;
+    // double eta=  0;
+    // double tise_tolerance = 1E-15;
+    // PetscInt tise_mat_iter = 3000;
+
+
+
+
     bool SAVE_BSPLINES = false;
+    bool EMBED = true;
 
 
     std::vector<std::complex<double>> knots = basis::linear_knots(n_basis, degree, rmax);
@@ -37,7 +51,7 @@ int main(int argc, char **argv) {
     PetscPrintf(PETSC_COMM_WORLD, "Solving TISE\n");
     double start_time = MPI_Wtime();
     
-    tise::solve_tise(n_basis,degree,knots,lmax,nmax,R0,eta,tise_tolerance,tise_mat_iter);
+    tise::solve_tise(n_basis,degree,knots,lmax,nmax,R0,eta,tise_tolerance,tise_mat_iter,EMBED);
 
     double end_time = MPI_Wtime();
     double elapsed_time = end_time - start_time;
--- TISE/source_files/tise.cpp
+++ TISE/source_files/tise.cpp
@@ -11,6 +11,24 @@
 namespace tise 
 {
 
+    PetscErrorCode save_matrix(Mat A, const char *filename)
+    {
+    PetscErrorCode ierr;
+    PetscViewer viewer;
+
+    // Open a binary viewer in write mode
+    ierr = PetscViewerBinaryOpen(PETSC_COMM_WORLD, filename, FILE_MODE_WRITE, &viewer); CHKERRQ(ierr);
+
+    // Write the matrix to the file in parallel
+    ierr = MatView(A, viewer); CHKERRQ(ierr);
+
+    // Clean up the viewer
+    ierr = PetscViewerDestroy(&viewer); CHKERRQ(ierr);
+
+    return ierr;
+    }
+
+
     PetscErrorCode construct_kinetic_matrix(Mat *A, int n_basis, int degree, const std::vector<std::complex<double>>& knots,double R0,double eta) 
     {
         PetscInt p_n_basis = static_cast<PetscInt>(n_basis);
@@ -235,7 +253,7 @@ namespace tise
         return 0; // Return success
     }
 
-    PetscErrorCode solve_tise(int n_basis,int degree,const std::vector<std::complex<double>>& knots,int lmax, int nmax,double R0, double eta,double tolerance, PetscInt max_iter)
+    PetscErrorCode solve_tise(int n_basis,int degree,const std::vector<std::complex<double>>& knots,int lmax, int nmax,double R0, double eta,double tolerance, PetscInt max_iter,bool EMBED)
     {
         PetscErrorCode ierr;
         PetscViewer viewTISE;
@@ -246,10 +264,68 @@ namespace tise
         Mat temp;
         EPS eps;
         PetscInt nconv;
-        
+        Mat H_total;
+        Mat S_total;
+        Mat I;
+        Mat I_partial;
+        Mat H_partial;
 
+        ierr = construct_overlap_matrix(&S, n_basis, degree, knots,R0,eta); CHKERRQ(ierr);
         
+        if (EMBED)
+        {
+            int total = 0;
+            for (int l = 0; l<=lmax; l++)
+            {
+                total += 2*l + 1;
+            }
+            int total_size = total * n_basis;
 
+            ierr = MatCreate(PETSC_COMM_WORLD, &I); CHKERRQ(ierr);
+            ierr = MatSetSizes(I,PETSC_DECIDE,PETSC_DECIDE,total,total); CHKERRQ(ierr);
+            ierr = MatSetFromOptions(I); CHKERRQ(ierr);
+            PetscInt nnz_per_row_identity = 1;
+            ierr = MatMPIAIJSetPreallocation(I,nnz_per_row_identity,NULL,nnz_per_row_identity,NULL); CHKERRQ(ierr);
+            ierr = MatSetUp(I); CHKERRQ(ierr);
+            PetscInt start_row,end_row;
+            ierr = MatGetOwnershipRange(I,&start_row,&end_row); CHKERRQ(ierr);
+
+            PetscInt nnz_per_row = 2*degree + 1;
+
+            ierr = MatCreate(PETSC_COMM_WORLD, &H_total); CHKERRQ(ierr);
+            ierr = MatSetSizes(H_total,PETSC_DECIDE,PETSC_DECIDE,total_size,total_size); CHKERRQ(ierr);
+            ierr = MatSetFromOptions(H_total); CHKERRQ(ierr);
+            ierr = MatMPIAIJSetPreallocation(H_total,nnz_per_row,NULL,nnz_per_row,NULL); CHKERRQ(ierr);
+            ierr = MatSetUp(H_total); CHKERRQ(ierr);
+            ierr = MatAssemblyBegin(H_total,MAT_FINAL_ASSEMBLY); CHKERRQ(ierr);
+            ierr = MatAssemblyEnd(H_total,MAT_FINAL_ASSEMBLY); CHKERRQ(ierr);
+
+            ierr = MatCreate(PETSC_COMM_WORLD, &S_total); CHKERRQ(ierr);
+            ierr = MatSetSizes(S_total,PETSC_DECIDE,PETSC_DECIDE,total_size,total_size); CHKERRQ(ierr);
+            ierr = MatSetFromOptions(S_total); CHKERRQ(ierr);
+            ierr = MatMPIAIJSetPreallocation(S_total,nnz_per_row,NULL,nnz_per_row,NULL); CHKERRQ(ierr);
+            ierr = MatSetUp(S_total); CHKERRQ(ierr);
+            ierr = MatAssemblyBegin(S_total,MAT_FINAL_ASSEMBLY); CHKERRQ(ierr);
+            ierr = MatAssemblyEnd(S_total,MAT_FINAL_ASSEMBLY); CHKERRQ(ierr);
+            
+           
+            for (PetscInt i = start_row; i< end_row; i++)
+            {
+                ierr = MatSetValue(I,i,i,1,INSERT_VALUES); CHKERRQ(ierr);
+            }
+           
+
+            ierr = MatAssemblyBegin(I,MAT_FINAL_ASSEMBLY); CHKERRQ(ierr);
+            ierr = MatAssemblyEnd(I,MAT_FINAL_ASSEMBLY); CHKERRQ(ierr);
+
+            ierr = MatSeqAIJKron(I,S,MAT_INITIAL_MATRIX,&S_total); CHKERRQ(ierr);
+            ierr = save_matrix(S_total,"S_total.dat"); CHKERRQ(ierr);
+
+            ierr = MatDestroy(&S_total); CHKERRQ(ierr);
+            ierr = MatDestroy(&I); CHKERRQ(ierr);
+        }
+        
+       
 
         ierr = PetscViewerHDF5Open(PETSC_COMM_WORLD, "tise_output.h5", FILE_MODE_WRITE, &viewTISE);
 
@@ -262,21 +338,81 @@ namespace tise
 
 
         ierr = construct_kinetic_matrix(&K, n_basis, degree, knots,R0,eta); CHKERRQ(ierr);
-        ierr = construct_overlap_matrix(&S, n_basis, degree, knots,R0,eta); CHKERRQ(ierr);
         ierr = construct_inv_r2_matrix(&Inv_r2, n_basis, degree,knots,R0,eta); CHKERRQ(ierr);
         ierr = construct_inv_r_matrix(&Inv_r,n_basis,degree,knots,R0,eta); CHKERRQ(ierr);
 
         for (int l=0; l<=lmax; ++l)
         {
+            ierr = MatDuplicate(K, MAT_COPY_VALUES, &temp); CHKERRQ(ierr);
+            ierr = MatAXPY(temp, l*(l+1)*0.5,Inv_r2,SAME_NONZERO_PATTERN); CHKERRQ(ierr);
+            ierr = MatAXPY(temp,-1.0,Inv_r,SAME_NONZERO_PATTERN); CHKERRQ(ierr);
+
+            if (EMBED)
+            {   
+
+                
+                int total = 0;
+                for (int l = 0; l<=lmax; l++)
+                {
+                    total += 2*l + 1;
+                }
+                int total_size = total * n_basis;
+
+
+                int start = 0;
+                for (int l_temp = 0; l_temp<l; l_temp++)
+                {
+                    start += 2*l_temp + 1;
+                }
+                int end = start + 2*l+1;
+
+
+                PetscInt nnz_per_row = 2*degree +1;
+                ierr = MatCreate(PETSC_COMM_WORLD, &H_partial); CHKERRQ(ierr);
+                ierr = MatSetSizes(H_partial,PETSC_DECIDE,PETSC_DECIDE,total_size,total_size); CHKERRQ(ierr);
+                ierr = MatSetFromOptions(H_total); CHKERRQ(ierr);
+                ierr = MatMPIAIJSetPreallocation(H_partial,nnz_per_row,NULL,nnz_per_row,NULL); CHKERRQ(ierr);
+                ierr = MatSetUp(H_partial); CHKERRQ(ierr);
+                ierr = MatAssemblyBegin(H_partial,MAT_FINAL_ASSEMBLY); CHKERRQ(ierr);
+                ierr = MatAssemblyEnd(H_partial,MAT_FINAL_ASSEMBLY); CHKERRQ(ierr);
+
+                ierr = MatCreate(PETSC_COMM_WORLD, &I_partial); CHKERRQ(ierr);
+                ierr = MatSetSizes(I_partial,PETSC_DECIDE,PETSC_DECIDE,total,total); CHKERRQ(ierr);
+                ierr = MatSetFromOptions(I_partial); CHKERRQ(ierr);
+                PetscInt nnz_per_row_identity = 1;
+                ierr = MatMPIAIJSetPreallocation(I_partial,nnz_per_row_identity,NULL,nnz_per_row_identity,NULL); CHKERRQ(ierr);
+                ierr = MatSetUp(I_partial); CHKERRQ(ierr);
+                PetscInt start_row,end_row;
+                ierr = MatGetOwnershipRange(I_partial,&start_row,&end_row); CHKERRQ(ierr);
+
+                for (PetscInt i = start_row; i< end_row; i++)
+                {
+                    if ((i >= start && i < end))
+                    {
+                        ierr = MatSetValue(I_partial,i,i,1,INSERT_VALUES); CHKERRQ(ierr);
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+
+                ierr = MatAssemblyBegin(I_partial,MAT_FINAL_ASSEMBLY); CHKERRQ(ierr);
+                ierr = MatAssemblyEnd(I_partial,MAT_FINAL_ASSEMBLY); CHKERRQ(ierr);
+
+                ierr = MatSeqAIJKron(I_partial,temp,MAT_INITIAL_MATRIX,&H_partial); CHKERRQ(ierr);
+
+                ierr = MatAXPY(H_total,1.0,H_partial,DIFFERENT_NONZERO_PATTERN); CHKERRQ(ierr);
+                ierr = MatDestroy(&H_partial); CHKERRQ(ierr);
+                ierr = MatDestroy(&I_partial); CHKERRQ(ierr);
+            }
+
             int num_of_energies = nmax - l;
             if (num_of_energies <= 0)
             {
                 continue;
             }
 
-            ierr = MatDuplicate(K, MAT_COPY_VALUES, &temp); CHKERRQ(ierr);
-            ierr = MatAXPY(temp, l*(l+1)*0.5,Inv_r2,SAME_NONZERO_PATTERN); CHKERRQ(ierr);
-            ierr = MatAXPY(temp,-1.0,Inv_r,SAME_NONZERO_PATTERN); CHKERRQ(ierr);
 
             ierr = EPSSetOperators(eps, temp, S); CHKERRQ(ierr);
             ierr = EPSSetDimensions(eps,num_of_energies,PETSC_DEFAULT,PETSC_DEFAULT);
@@ -335,6 +471,9 @@ namespace tise
         
         }
 
+
+        ierr = save_matrix(H_total,"H_total.dat"); CHKERRQ(ierr);
+
         ierr = PetscViewerDestroy(&viewTISE); CHKERRQ(ierr);
         ierr = EPSDestroy(&eps); CHKERRQ(ierr);
         ierr = MatDestroy(&K); CHKERRQ(ierr);
@@ -342,6 +481,7 @@ namespace tise
         ierr = MatDestroy(&Inv_r); CHKERRQ(ierr);
         ierr = MatDestroy(&S); CHKERRQ(ierr);
         ierr = MatDestroy(&temp); CHKERRQ(ierr);
+        ierr = MatDestroy(&H_total); CHKERRQ(ierr);
 
 
         return 0;
